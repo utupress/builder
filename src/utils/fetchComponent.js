f6a61b8e9ba464e139a270bb73d9527b01e8dc96
Dedan Irungu <dedanirungu@gmail.com>
2022-12-02 11:55:54 +0300

    changes

--- src/utils/fetchComponent.js
+++ src/utils/fetchComponent.js
@@ -10,18 +10,19 @@ window.$filters = filters;
 window.$func = filters;
 window.$methods = filters;
 
+/*
 const options = {
     moduleCache: {
         vue: Vue
     },
     async getFile(url) {
-        
+
         console.log(url)
         const res = await fetch(url);
         if (!res.ok)
-        return {
-            getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
-        }
+            return {
+                getContentData: asBinary => asBinary ? res.arrayBuffer() : res.text(),
+            }
     },
     addStyle(textContent) {
 
@@ -42,7 +43,7 @@ const options = {
         set(key, str) {
 
             // naive storage space management
-            for (;;) {
+            for (; ;) {
 
                 try {
 
@@ -69,6 +70,24 @@ const options = {
             return JSON.parse(source);
     }
 
+}
+*/
+
+const options = {
+    moduleCache: {
+        vue: Vue
+    },
+    async getFile(url) {
+        const res = await fetch(url)
+        if (!res.ok)
+            throw Object.assign(new Error(`${res.statusText} ${url}`), { res })
+        return await res.text();
+    },
+    addStyle(textContent) {
+        const style = Object.assign(document.createElement("style"), { textContent })
+        const ref = document.head.getElementsByTagName("style")[0] || null
+        document.head.insertBefore(style, ref)
+    },
 }
 
 const fetchComponent = (comp_path) => {
